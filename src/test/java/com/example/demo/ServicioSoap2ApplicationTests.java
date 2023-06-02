40852e7832800ce2fdf38e5d535646f0907cf3c8
kevin Diaz <diazsosa16@gmail.com>
2023-06-02 15:55:12 -0500

    ProjectRest

---
+++ src/test/java/com/example/demo/ServicioSoap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioSoap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
